2477ae0e6480ed1a6e117892c0dceac8c5947519
Chandan Mahanta <mahantachandankumar769@gmail.com>
2026-04-27 21:07:46 +0530

    fix cors issue

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors({ origin: CLIENT_URL, credentials: true }));
+// app.use(cors({ origin: CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
